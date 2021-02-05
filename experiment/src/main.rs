55ff20c13f9d1f32ad5fe921f655e2011ad1e13e
phil-markarian <phil.markarian@gmail.com>
2021-02-05 13:56:11 +0900

    add files

--- experiment/src/main.rs
+++ experiment/src/main.rs
@@ -7,6 +7,25 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/new")]
+fn new() -> &'static str {
+    "Hello, new!"
+}
+
+#[get("/hello/<name>/<age>/<cool>")]
+fn hello(name: String, age: u8, cool: bool) -> String {
+    if cool {
+        format!("You're a cool {} year old, {}!", age, name)
+    } else {
+        format!("{}, we need to talk about your coolness.", name)
+    }
+}
+
+#[get("/test/<test>")]
+fn test(test: String) -> String {
+    format!("This is a test {}!", test)
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/", routes![index, new, hello, test]).launch();
 }
